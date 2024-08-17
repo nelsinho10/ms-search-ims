6dd841256f509cfc9ffc0a76a7018ac519b65fca
Nelson Sambula <nelson.sambula@movitext.com>
2024-08-17 13:11:53 -0600

    chore: Add initial project files and configuration

---
+++ src/main/java/com/unir/ms_search/MsSearchApplication.java
@@ -0,0 +1,13 @@
+package com.unir.ms_search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsSearchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unir/ms_search/MsSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unir.ms_search;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
